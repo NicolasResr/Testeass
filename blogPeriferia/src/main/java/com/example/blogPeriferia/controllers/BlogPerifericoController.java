bedc5c2a5029c9e97d61dcb2ae01cc601cf42449
NicolasResr <npereirasantosdantas@gmail.com>
2025-02-25 16:56:30 -0300

    Add files via upload

--- blogPeriferia/src/main/java/com/example/blogPeriferia/controllers/BlogPerifericoController.java
+++ blogPeriferia/src/main/java/com/example/blogPeriferia/controllers/BlogPerifericoController.java
@@ -1,14 +1,28 @@
 package com.example.blogPeriferia.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BlogPerifericoController {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
 	@GetMapping("/hello")
 	public String helloWorld() {
 		return "Hello, World!";
 	}
 
+	@GetMapping("/verificar")
+	public String verificarBancoDeDados() {
+		try {
+			jdbcTemplate.execute("SELECT 1");
+			return "Conexão com o banco de dados bem-sucedida!";
+		} catch (Exception e) {
+			return "Falha ao conectar com o banco de dados: " + e.getMessage();
+		}
+	}
 }
